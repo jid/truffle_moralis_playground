6eea8ccb99ab1a3e8d844f8078b57b04357b7dd8
jid <idczak.jaroslaw@gmail.com>
2021-06-21 14:17:33 +0200

    Contracts interaction

--- migrations/2_hello_world_migration.js
+++ migrations/2_hello_world_migration.js
@@ -1,5 +1,10 @@
 const HelloWorld = artifacts.require("HelloWorld");
+const HelloWorldPermanent = artifacts.require("HelloWorldPermanent");
 
 module.exports = function (deployer) {
-  deployer.deploy(HelloWorld, "Hello world constructor");
+  deployer.deploy(HelloWorld, "Hello world constructor").then( async () => {
+    let instance = await HelloWorld.deployed();
+    let message = instance.message();
+    return deployer.deploy(HelloWorldPermanent, message);
+  });
 };
